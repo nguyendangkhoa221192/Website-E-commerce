84e1aba6e6956d6730acec8158565d68c3de5708
Nguyen Dang Khoa <khoand@vccvn.com>
2017-12-07 16:18:22 +0700

    Update code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'home_page/index'
+  root 'home_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
