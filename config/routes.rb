34efa4e525253658451b562a4062440d3ea9a364
Nguyen Dang Khoa <khoand@vccvn.com>
2017-12-15 11:48:33 +0700

    + Update new code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home_page/index'
-  root 'home_page#index'
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- vendor/ruby/2.3.0/bin/scss.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\RailsInstaller\Ruby2.3.0\bin\ruby.exe" "E:/khoand/work_out_time/Website-E-commerce/vendor/ruby/2.3.0/bin/scss" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"C:\RailsInstaller\Ruby2.3.0\bin\ruby.exe" "%~dpn0" %*
